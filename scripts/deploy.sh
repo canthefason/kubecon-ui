8c56bd8e21c124a5e7238880661c1cdd976352eb
Can Yucel <can.yucel@gmail.com>
2016-03-06 16:27:01 -0800

    add scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+kubeargs="--namespace=$KUBE_ENV"
+
+shopt -s expand_aliases
+alias kubectl="sudo docker run --rm -v /home/core/.kube:/root/.kube -v /home/core/pipelines/nginx:/root/nginx wernight/kubectl kubectl"
+
+set +e
+rcExist=$(kubectl get -o template rc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
+set -e
+if [ "$rcExist" != "ReplicationController" ]; then
+  kubectl create -f /root/nginx/scripts/rc.yml $kubeargs
+else
+  kubectl rolling-update $GO_PIPELINE_NAME --image=canthefason/$GO_PIPELINE_NAME:$GO_PIPELINE_LABEL --update-period=20s $kubeargs
+fi
+
+set +e
+svcExist=$(kubectl get -o template svc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
+set -e
+if [ "$svcExist" != "Service" ]; then
+  kubectl create -f /root/nginx/scripts/svc.yml $kubeargs
+fi
