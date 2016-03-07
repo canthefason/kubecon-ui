f179613c01fe9c26417187af3e9d7f6bf90ef106
Can Yucel <can.yucel@gmail.com>
2016-03-06 16:39:18 -0800

    add tag name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,13 @@ kubeargs="--namespace=$KUBE_ENV"
 shopt -s expand_aliases
 alias kubectl="sudo docker run --rm -v /home/core/.kube:/root/.kube -v /home/core/pipelines/nginx:/root/nginx wernight/kubectl kubectl"
 
+
 set +e
 rcExist=$(kubectl get -o template rc $GO_PIPELINE_NAME --template={{.kind}} $kubeargs)
 set -e
 if [ "$rcExist" != "ReplicationController" ]; then
+  tag=$GO_PIPELINE_NAME:$GO_PIPELINE_LABEL
+  sed -i -e "s/\${TAG}/$tag/g" scripts/rc.yaml
   kubectl create -f /root/nginx/scripts/rc.yml $kubeargs
 else
   kubectl rolling-update $GO_PIPELINE_NAME --image=canthefason/$GO_PIPELINE_NAME:$GO_PIPELINE_LABEL --update-period=20s $kubeargs
